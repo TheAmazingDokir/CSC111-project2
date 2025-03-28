24fa6041c853cae4e69530e58d37f60a1bd23d0f
404Haze <108695733+404Haze@users.noreply.github.com>
2025-03-27 21:02:46 -0400

    created stats file

---
+++ stats.py
@@ -0,0 +1,23 @@
+import math as m
+import numpy as np
+
+
+# Available Info:
+# Name
+# Daily minutes on site
+# Daily pageviews per visitor
+# Ratio of traffic from search
+# Total sites linking in
+
+# Potential stats to calculate:
+# Website success rating (come complex formula using previously calculated stats)
+# Average time per page (w/ daily minutes on site & daily pageviews per visitor)
+# Search impact factor (w/ ratio of traffic from search & total sites linking in)
+# Engagement density (w/ daily min on site & daily pageviews per visitor)
+# Search efficiency score (w/ daily min on site & ratio of traffic from search & daily pageviews per visitor)
+# Search conversion potential (w/ ratio of traffic from search & total sites linking in)
+# Search & link synergy (w/ ratio of traffic from search & total sites linking in & daily minutes on site)
+# Visitor impact score (w/ daily min on site & daily pageviews per visitor & total sites linking in)
+
+
+print("nothing to see here yet")
