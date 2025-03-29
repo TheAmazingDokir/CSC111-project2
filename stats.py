6f8163a01dbaf051711c4f491c4c0f00f548afc6
404Haze <108695733+404Haze@users.noreply.github.com>
2025-03-29 15:48:08 -0400

    did stats layout

--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 import math as m
 import numpy as np
-
+import class_graph
 
 # Available Info:
 # Name
@@ -20,4 +20,60 @@ import numpy as np
 # Visitor impact score (w/ daily min on site & daily pageviews per visitor & total sites linking in)
 
 
-print("nothing to see here yet")
+
+
+
+    # Layout:
+
+    # _Vertex:
+    # item: Any
+    # neighbours: set[_Vertex]
+    # links_in: set[_Vertex]
+    # links_out: set[_Vertex]
+    # stats: dict[str, Any]
+
+    # Directed_Graph:
+    # _vertices: dict[Any, _Vertex]
+    # _edges: dict[tuple[Any, Any], dict[str, Any]]
+
+
+
+class Directed_Graph:
+
+    # Calculating global stats
+
+    def calc_global_daily_min() -> None:
+        """Calculates the global average for daily minutes on site."""
+        pass
+
+    def calc_global_daily_pageviews() -> None:
+        """Calculates the global average for daily pageviews per visitor."""
+        pass
+
+    def calc_global_traffic_ratio() -> None:
+        """Calculates the global average for traffic ratio from search."""
+        pass
+    
+    def calc_global_site_links() -> None:
+        """Calculates the global average for site linking in."""
+        pass
+
+    # more to be added...
+
+
+
+class _Vertex:
+
+    # Additional stats
+
+
+    # coming soon....
+
+
+
+
+    # Final success rating
+
+    def calc_success_rating() -> None:
+        """Calculates the overall success rating for the website based on present statistics."""
+        pass
