51b6107dee29a072bc61e55d2095518d65a7e386
Sam2836 <samiacobazzi@gmail.com>
2025-03-31 15:35:09 -0400

    real commit before merge; update lamda labels

--- main.py
+++ main.py
@@ -65,24 +65,15 @@ stats.loader(webgraph, stats.predict_rank)
 # for v in webgraph.get_vertices():
 #     normalized_degree = math.erf(len(webgraph.get_website_neighbours()))
 
-# name = lambda v: (f"Website: {v.domain_name}<br>"+
-#                     f"Avg daily min: {v.stats['daily_min']}%<br>"+
-#                     f"Daily pageviews per visitor: {v.stats['daily_pageviews']}%<br>"+
-#                     f"Min per page: {v.stats['min_per_page']}%<br>"+
-#                     f"Search traffic: {v.stats['search_traffic']}%<br>"+
-#                     f"Total sites linking in: {v.stats['site_links']}<br>"+
-#                     f"Links traffic: {v.stats['links_traffic']}%<br>"+
-#                     f"Engagement rating: {v.stats['engagement_rating']}%<br>"+
-#                     f"Predicted rank: {v.stats['predicted_rank']}%")
 name = lambda v: (f"Website: {v.domain_name}<br>"+
-                    f"Avg daily min: {v.stats['daily_min']} ({stats.percentify(v.stats['daily_min'], stats.calc_global_daily_min, webgraph)}% global)<br>"+
-                    f"Daily pageviews per visitor: {v.stats['daily_pageviews']}<br>"+
-                    f"Min per page: {v.stats['min_per_page']}<br>"+
-                    f"Search traffic: {v.stats['search_traffic']}%<br>"+
-                    f"Total sites linking in: {v.stats['site_links']}<br>"+
-                    f"Links traffic: {v.stats['links_traffic']}%<br>"+
-                    f"Engagement rating: {v.stats['engagement_rating']}<br>"+
-                    f"Predicted rank: {v.stats['predicted_rank']}")
+                    f"Avg Daily Min: {v.stats['daily_min']} ({stats.percentify(v.stats['daily_min'], stats.calc_global_daily_min, webgraph)}% global)<br>"+
+                    f"Daily Unique Pageviews: {v.stats['daily_pageviews']} ({stats.percentify(v.stats['daily_pageviews'], stats.calc_global_daily_pageviews, webgraph)}% global)<br>"+
+                    f"Avg Min Per Page: {v.stats['min_per_page']} ({stats.percentify(v.stats['min_per_page'], stats.calc_global_min_per_page, webgraph)}% global)<br>"+
+                    f"Search Traffic Impact: {v.stats['search_traffic']}% ({stats.percentify(v.stats['search_traffic'], stats.calc_global_search_traffic, webgraph)}% global)<br>"+
+                    f"Total Sites Linking-in: {v.stats['site_links']} ({stats.percentify(v.stats['site_links'], stats.calc_global_site_links, webgraph)}% global)<br>"+
+                    f"Links Traffic Impact: {v.stats['links_traffic']}% ({stats.percentify(v.stats['links_traffic'], stats.calc_global_links_traffic, webgraph)}% global)<br>"+
+                    f"Engagement Rating: {v.stats['engagement_rating']} ({stats.percentify(v.stats['engagement_rating'], stats.calc_global_engagement_rating, webgraph)}% global)<br>"+
+                    f"Predicted Rank: {v.stats['predicted_rank']}")
 
 x = [len(v.links_in) for v in webgraph.get_vertices()]
 y = [v.stats["engagement_rating"] for v in webgraph.get_vertices()]
@@ -115,9 +106,9 @@ fig.show()
     
 
 
-visual.launch_web_graph(webgraph, 10000)
+# visual.launch_web_graph(webgraph, 10000)
 
-tkw.launch_control_panel(webgraph, 10000)
+# tkw.launch_control_panel(webgraph, 10000)
 
 
 
--- visual.py
+++ visual.py
@@ -37,24 +37,15 @@ def launch_web_graph(webgraph: cg.Webgraph, n_vertices: int) -> None:
     # Label function
     vertex_dict = {v.domain_name: v for v in webgraph.get_vertices()}
 
-    # name = lambda v: (f"Website: {v.domain_name}<br>"+
-    #                 f"Avg daily min: {v.stats['daily_min']}%<br>"+
-    #                 f"Daily pageviews per visitor: {v.stats['daily_pageviews']}%<br>"+
-    #                 f"Min per page: {v.stats['min_per_page']}%<br>"+
-    #                 f"Search traffic: {v.stats['search_traffic']}%<br>"+
-    #                 f"Total sites linking in: {v.stats['site_links']}<br>"+
-    #                 f"Links traffic: {v.stats['links_traffic']}%<br>"+
-    #                 f"Engagement rating: {v.stats['engagement_rating']}%<br>"+
-    #                 f"Predicted rank: {v.stats['predicted_rank']}%")
     name = lambda v: (f"Website: {v.domain_name}<br>"+
-                    f"Avg daily min: {v.stats['daily_min']} ({stats.percentify(v.stats['daily_min'], stats.calc_global_daily_min, webgraph)}% global)<br>"+
-                    f"Daily pageviews per visitor: {v.stats['daily_pageviews']}<br>"+
-                    f"Min per page: {v.stats['min_per_page']}<br>"+
-                    f"Search traffic: {v.stats['search_traffic']}%<br>"+
-                    f"Total sites linking in: {v.stats['site_links']}<br>"+
-                    f"Links traffic: {v.stats['links_traffic']}%<br>"+
-                    f"Engagement rating: {v.stats['engagement_rating']}<br>"+
-                    f"Predicted rank: {v.stats['predicted_rank']}")
+                    f"Avg Daily Min: {v.stats['daily_min']} ({stats.percentify(v.stats['daily_min'], stats.calc_global_daily_min, G)}% global)<br>"+
+                    f"Daily Unique Pageviews: {v.stats['daily_pageviews']} ({stats.percentify(v.stats['daily_pageviews'], stats.calc_global_daily_pageviews, G)}% global)<br>"+
+                    f"Avg Min Per Page: {v.stats['min_per_page']} ({stats.percentify(v.stats['min_per_page'], stats.calc_global_min_per_page, G)}% global)<br>"+
+                    f"Search Traffic Impact: {v.stats['search_traffic']}% ({stats.percentify(v.stats['search_traffic'], stats.calc_global_search_traffic, G)}% global)<br>"+
+                    f"Total Sites Linking-in: {v.stats['site_links']} ({stats.percentify(v.stats['site_links'], stats.calc_global_site_links, G)}% global)<br>"+
+                    f"Links Traffic Impact: {v.stats['links_traffic']}% ({stats.percentify(v.stats['links_traffic'], stats.calc_global_links_traffic, G)}% global)<br>"+
+                    f"Engagement Rating: {v.stats['engagement_rating']} ({stats.percentify(v.stats['engagement_rating'], stats.calc_global_engagement_rating, G)}% global)<br>"+
+                    f"Predicted Rank: {v.stats['predicted_rank']}")
     # Extract node coordinates and tooltips
     node_x, node_y, tooltips = [], [], []
     for node in G.nodes():
